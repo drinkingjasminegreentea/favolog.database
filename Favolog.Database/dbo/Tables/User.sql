ee12e61af6525dac19ca7906097c949df75c8297
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-13 11:00:07 -0600

    adding username back

--- Favolog.Database/dbo/Tables/User.sql
+++ Favolog.Database/dbo/Tables/User.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,	
+    [Username] NVARCHAR(100) NULL, 
+    [EmailAddress] VARCHAR(250) NULL,    
 	[ProfileImage] VARCHAR(200) NULL, 
     [FirstName] NVARCHAR(100) NULL,
-    [LastName] NVARCHAR(100) NULL, 
-    [EmailAddress] VARCHAR(250) NULL,    
+    [LastName] NVARCHAR(100) NULL,     
     [Bio] VARCHAR(250) NULL,    
     [Website] VARCHAR(250) NULL,
     [ExternalId] VARCHAR(50) NOT NULL, 
--- Favolog.Database/dbo/Views/UserFeed.sql
+++ Favolog.Database/dbo/Views/UserFeed.sql
@@ -1,5 +1,5 @@
 ﻿CREATE VIEW [dbo].[vw_UserFeed]
-AS SELECT CatalogId, Catalog.Name as CatalogName, UserId, [User].FirstName, [User].LastName, [User].ProfileImage, Item.Id, Title, ImageName, Url, 
+AS SELECT CatalogId, Catalog.Name as CatalogName, UserId, [User].Username, [User].ProfileImage, Item.Id, Title, ImageName, Url, 
 Comment, AudienceType
 FROM dbo.Catalog
 INNER JOIN dbo.Item ON Item.CatalogId = Catalog.Id
