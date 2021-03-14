e47794faf3857aeab441b019ec426fdcd2c76287
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-03-13 21:10:40 -0600

    fixing bio

--- Favolog.Database/dbo/Tables/User.sql
+++ Favolog.Database/dbo/Tables/User.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,	
-    [Username] NVARCHAR(100) NULL, 
+    [Username] VARCHAR(30) NOT NULL, 
     [EmailAddress] VARCHAR(250) NULL,    
 	[ProfileImage] VARCHAR(200) NULL, 
     [FirstName] NVARCHAR(100) NULL,
     [LastName] NVARCHAR(100) NULL,     
-    [Bio] VARCHAR(250) NULL,    
+    [Bio] NVARCHAR(250) NULL,    
     [Website] VARCHAR(250) NULL,
     [ExternalId] VARCHAR(50) NOT NULL, 
     CONSTRAINT [PK_User] PRIMARY KEY ([Id])    
@@ -14,3 +14,6 @@
 
 GO
 CREATE UNIQUE INDEX [IX_User_ExternalId] ON [dbo].[User] ([ExternalId])
+
+GO
+CREATE UNIQUE INDEX [IX_User_Username] ON [dbo].[User] ([Username])
