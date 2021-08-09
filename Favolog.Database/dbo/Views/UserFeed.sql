1473bb642e192665df1ac7ccf5e45d47495bca57
drinkingjasminegreentea <36995715+drinkingjasminegreentea@users.noreply.github.com>
2021-08-09 08:55:13 -0500

    added source image url

--- Favolog.Database/dbo/Views/UserFeed.sql
+++ Favolog.Database/dbo/Views/UserFeed.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[vw_UserFeed]
 AS SELECT CatalogId, Catalog.Name as CatalogName, UserId, [User].Username, [User].ProfileImage, Item.Id, Title, ImageName, Url, 
-Comment
+Comment, Item.SourceImageUrl
 FROM dbo.Catalog
 INNER JOIN dbo.Item ON Item.CatalogId = Catalog.Id
 INNER JOIN dbo.[User] ON Catalog.UserId = [User].Id
